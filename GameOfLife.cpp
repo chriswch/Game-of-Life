93ee12f5ca0f9a9e60673171d0da1cff68fbf6c0
chriswch <ChrisWong@csie.io>
2021-09-29 15:56:43 +0800

    modified the indentation

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -29,12 +29,12 @@ class GameOfLife
 	public:
 		GameOfLife();							//default
 		GameOfLife(const int w, const int h);	//initialize size
-		void initialize(const int p);	// initialize pattern (1~100)
-		void proceed(const int times);	// run "times" times (default 1)
-		void display();					//show result
+		void initialize(const int p);			// initialize pattern (1~100)
+		void proceed(const int times);			// run "times" times (default 1)
+		void display();							//show result
 	private:
-		int width;	// 16~256
-		int height;	// 26~256
+		int width;				// 16~256
+		int height;				// 26~256
 		int pattern;			// specify original pattern
 		bool cell[256][256];	// map size
 };
